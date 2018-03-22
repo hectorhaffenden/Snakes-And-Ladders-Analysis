7c096b7913655c93e7862700cd269498f37f1a7c
Hector Haffenden <36663323+hectorhaffenden@users.noreply.github.com>
2018-03-22 21:09:39 +0000

    Delete AverageMovesToWin.R

--- SnakesAndLaddersAnalysis/R/AverageMovesToWin.R
+++
@@ -1,7 +0,0 @@
-AverageMovesToWin <- function(number.of.players = 2, number.of.trials = 1000){
-  x <- 0
-  for (i in 1:number.of.trials){
-    x <- x + nrow(SnakesAndLaddersGame(number.of.players))
-  }
-  x/number.of.trials
-}
